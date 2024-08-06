5e96c99e2d9bee71333c5ac972642e66d27b869a
hasim7025 <62057506+hasim7025@users.noreply.github.com>
2024-08-06 12:23:38 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default {
+  output: 'export',
+  // Add the basePath to match your GitHub Pages URL structure
+  basePath: '/github-actions-with-react',
+  images: {
+    unoptimized: true,
+  },
+}
